9e9eced6125e68008b40c8709cfae8335e99a9a3
lambtsta <maryjoyce.llabres@g.batstate-u.edu.ph>
2023-06-21 16:23:21 +0800

    Updated

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -8,19 +8,19 @@ import { DomSanitizer } from '@angular/platform-browser';
 })
 export class AdminComponent {
   myAngularxQrCode: any;
-  showContent1 = false;
-  showContent2 = true;
+  showContent1 = true;
+  showContent2 = false;
   qrValue: string = 'Hello, World!'; 
   showContent3 = false;
   name: any;
   profile: any;
   isHidden: boolean = true;
-
+  tableData:any = [];
 
   fileUrl: any;
   constructor(private sanitizer: DomSanitizer) {  }
   ngOnInit(): void{
-    this.myAngularxQrCode = 'Name:Ron Iverson Del Mundo \n Age:20 \n Gender: Male \n School:BSU \n From: Talaga East \n Image Url: https://drive.google.com/file/d/1APg0hfuOyp1DcaNuWXIJKHipSIaHVxwM/view \n FAR Url: https://drive.google.com/file/d/1Ao9jpjNE0gWD4AW3ig1WUSXH2Om6VB-a/view';
+    this.myAngularxQrCode = 'Item Code: 20-06186 \n Name:Accent Chair \n Category:Furniture \n Project: N/A \n Location: CTI \n Specific: Co-Working Space \n Condition: Working \n Image Url: https://drive.google.com/file/d/1AXRE19U2k9bZW459wVJfeCPPra8ecbtZ/view?usp=sharing \n FAR Url: https://drive.google.com/file/d/1Ao9jpjNE0gWD4AW3ig1WUSXH2Om6VB-a/view';
     const data = 'ewqewqeqw';
     const blob = new Blob([data], { type: 'application/octet-stream' });
     
@@ -40,6 +40,11 @@ export class AdminComponent {
     });
     this.name = localStorage.getItem('name')
     this.profile = localStorage.getItem('position')?.toUpperCase();
+   
+  }
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+    // Do further processing with the selected file
   }
   toggleContent(contentId: string): void {
     if (contentId === 'content1') {
@@ -50,6 +55,38 @@ export class AdminComponent {
       this.showContent1 = false;
       this.showContent2 = true;
       this.showContent3 = false;
+      const formData = new FormData();
+   
+      fetch('http://localhost:8080/IMS/src/backend/itemlist.php', {
+      method: 'POST',
+      body: formData
+    })
+    .then(response => response.json())
+    .then(value => {
+      console.log(value.data[0]);
+      for (let i = 0; i < value.data.length; i++) {
+        const codeValue = value.data[i].item_id !== '' ? value.data[i].item_id : 'N/A';
+        const productValue = value.data[i].item_name !== '' ? value.data[i].item_name  : 'N/A';
+        const categoryValue = value.data[i].category !== '' ? value.data[i].category : 'N/A';
+  
+        const locationValue = value.data[i].location !== '' ? value.data[i].location : 'N/A';
+  
+        const projectValue = value.data[i].project !== '' ? value.data[i].project : 'N/A';
+  
+        const conditionValue = value.data[i].state !== '' ? value.data[i].state : 'N/A';
+
+        this.tableData[i] = {
+          code:  codeValue ,
+          productname: productValue,
+          category: categoryValue,
+          location: locationValue,
+          project: projectValue,
+          condition: conditionValue
+        };
+   // Access and log the "code" property
+      }
+ 
+     });
     }
     else if (contentId === 'content3') {
       this.showContent1 = false;
@@ -79,17 +116,52 @@ closeButton.addEventListener('click', () => {
   popupFormContainer.style.display = 'none';
 });
 
-// Handle form submission
-const myForm = document.getElementById('myForm') as HTMLInputElement;
-myForm.addEventListener('submit', (event) => {
-  event.preventDefault(); // Prevent default form submission
-  // Here, you can perform further actions like sending the form data to a server
-  console.log('Form submitted');
+    // Handle form submission
+    const myForm = document.getElementById('myForm') as HTMLInputElement;
+    myForm.addEventListener('submit', (event) => {
+      event.preventDefault(); // Prevent default form submission
+      // Here, you can perform further actions like sending the form data to a server
+      console.log('Form submitted');
 });
   }
   submit(){
     const itemcode = document.getElementById('itemcode') as HTMLInputElement;
     console.log(itemcode.value);
     this.isHidden = !this.isHidden;
+  }
+  refresh(){
+    const formData = new FormData();
+    fetch('http://localhost:8080/IMS/src/backend/itemlist.php', {
+      method: 'POST',
+      body: formData
+    })
+    .then(response => response.json())
+    .then(value => {
+      console.log(value.data[0]);
+      for (let i = 0; i < value.data.length; i++) {
+        const codeValue = value.data[i].item_id !== '' ? value.data[i].item_id : 'N/A';
+        const productValue = value.data[i].item_name !== '' ? value.data[i].item_name  : 'N/A';
+        const categoryValue = value.data[i].category !== '' ? value.data[i].category : 'N/A';
+  
+        const locationValue = value.data[i].location !== '' ? value.data[i].location : 'N/A';
+  
+        const projectValue = value.data[i].project !== '' ? value.data[i].project : 'N/A';
+  
+        const conditionValue = value.data[i].state !== '' ? value.data[i].state : 'N/A';
+  
+        this.tableData[i] = {
+          code:  codeValue ,
+          productname: productValue,
+          category: categoryValue,
+          location: locationValue,
+          project: projectValue,
+          condition: conditionValue
+        };
+   // Access and log the "code" property
+      }
+ 
+     });
+    location.reload();
+    
   }
 }
